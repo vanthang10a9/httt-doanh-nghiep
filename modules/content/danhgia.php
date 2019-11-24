1f0e57e4f06335c1de2433721ac861ce2f6e6692
vanthang10a9 <levanthang510@gmail.com>
2019-11-25 05:38:58 +0700

    * tách đoạn html đánh giá ra làm 1 file để dễ dùng lại

---
+++ modules/content/danhgia.php
@@ -0,0 +1,97 @@
+<section class="ftco-section testimony-section">
+    <div class="container">
+        <div class="row justify-content-center mb-5 pb-3">
+            <div class="col-md-7 heading-section ftco-animate text-center">
+                <span class="subheading">Đánh giá</span>
+                <h2 class="mb-4">Đánh giá hài lòng của khách hàng</h2>
+                <p>Xa xa mùi gì lạ lùng, lùng sục một hồi thì ra pizza, Pizza thì ở quanh đây, Tìm xa chi rứa, không thấy mệt à</p>
+            </div>
+        </div>
+        <div class="row ftco-animate">
+            <div class="col-md-12">
+                <div class="carousel-testimony owl-carousel">
+                    <div class="item">
+                        <div class="testimony-wrap p-4 pb-5">
+                            <div class="user-img mb-5" style="background-image: url(images/person_1.jpg)">
+                                <span class="quote d-flex align-items-center justify-content-center">
+                                    <i class="icon-quote-left"></i>
+                                </span>
+                            </div>
+                            <div class="text text-center">
+                                <p class="mb-5 pl-4 line">Tôi đánh giá cao chât lượng pizza tại đây, nó mang hơi hơi hướng phương tây,
+                                    nhưng vẫn ko lấn át mất hương vị truyền thống. Đây thực sự là một nơi đáng để bạn thử ít nhất 1 lần.
+                                </p>
+                                <p class="name">Hồ Hồng Phúc</p>
+                                <span class="position">Chuyên viên báo cáo</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="testimony-wrap p-4 pb-5">
+                            <div class="user-img mb-5" style="background-image: url(images/person_2.jpg)">
+                                <span class="quote d-flex align-items-center justify-content-center">
+                                    <i class="icon-quote-left"></i>
+                                </span>
+                            </div>
+                            <div class="text text-center">
+                                <p class="mb-5 pl-4 line">Nó ngon phải nói là ở tầm vũ trụ các bạn ạ. Tôi đã nghiện ngay từ lần đầu tiên thưởng thức
+                                    và bạn bè tôi cũng vậy. Tôi nghĩ bạn cũng nên rủ bạn bè cùng thưỡng thức ngay đi thôi.
+                                </p>
+                                <p class="name">Trần Quốc Thắng</p>
+                                <span class="position">Cố vấn dữ liệu</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="testimony-wrap p-4 pb-5">
+                            <div class="user-img mb-5" style="background-image: url(images/person_3.jpg)">
+                                <span class="quote d-flex align-items-center justify-content-center">
+                                    <i class="icon-quote-left"></i>
+                                </span>
+                            </div>
+                            <div class="text text-center">
+                                <p class="mb-5 pl-4 line">Tôi đánh giá cao dịch vụ tại đây. Nó hoàn hảo đến mức tôi không có gì phàn nàn.
+                                    Chiến dịch quảng cáo, chất lượng sản phẩm, dịch cụ giao hàng và chăm sóc khách hàng, ... tôi chưa thấy sự bất tiện nào
+                                    ngoại trừ website còn chưa hoàn thiện lắm, nhưng tui nghĩ như thế là quá đủ cho 5 sao rồi.
+                                </p>
+                                <p class="name">Nguyễn Thái Thuận</p>
+                                <span class="position">Leader hệ thống thông tin</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="testimony-wrap p-4 pb-5">
+                            <div class="user-img mb-5" style="background-image: url(images/person_1.jpg)">
+                                <span class="quote d-flex align-items-center justify-content-center">
+                                    <i class="icon-quote-left"></i>
+                                </span>
+                            </div>
+                            <div class="text text-center">
+                                <p class="mb-5 pl-4 line">Far far away, behind the word mountains, far from the
+                                    countries Vokalia and Consonantia, there live the blind texts.</p>
+                                <p class="name">Garreth Smith</p>
+                                <span class="position">Web Developer</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="testimony-wrap p-4 pb-5">
+                            <div class="user-img mb-5" style="background-image: url(images/person_1.jpg)">
+                                <span class="quote d-flex align-items-center justify-content-center">
+                                    <i class="icon-quote-left"></i>
+                                </span>
+                            </div>
+                            <div class="text text-center">
+                                <p class="mb-5 pl-4 line">Nhân viên rất nhiệt tình và năng nổ. Tôi thích cách quản lí của cửa hàng. Dịch vụ online cũng rất 
+                                    là ok. Phải nói là website thiết kế nhìn khá là mát mắt đối với người dùng. Đó là một điểm cộng lớn cho cửa hàng của các bạn.
+                                </p>
+                                <p class="name">Trần Anh Tuấn</p>
+                                <span class="position">Giám đốc cty mã nguồn Web</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
