c69034bf8f782d36b8031c2665507ca87dbdeb56
vanthang10a9 <levanthang510@gmail.com>
2019-12-03 22:29:47 +0700

    + làm đăng kí (có lưu session['username']), nhưng chưa hiện thịtrên header thông tin rằng đã đăng nhập

--- admin/congcu/login.php
+++ admin/congcu/login.php
@@ -13,26 +13,26 @@
 
     $password=md5($password);
 
-    echo $_SESSION['username'];
     if($result=DataProvider::executeQuery('SELECT * FROM taikhoan')){
         if ($result != null) {
             while ($row = mysqli_fetch_array($result)) {
                 if($row['USERNAME']==$username){
                     if($row['PASSWORD']==$password){
-                        echo "Đăng nhập thành công !";
+                        echo "Đăng nhập thành công ! <a href='../../../HTTT/index.php'>Về trang chủ</a>";
                         $_SESSION['username']=$username;
-                        header("Location: ../../index.php");
+                        //header("Location: ../../index.php"); //điều hướng tự chuyển hướng về home
                         break;
                     }else{
-                        echo "Password không đúng !";
-                        //header("Location: login.html?status=false");
+                        echo "Password không đúng !  <a href='javascript: history.go(-1)'>Trở lại</a>";
+                       
+                        //header("Location: login.html?status=false"); //tự chuyển về lại trang login.html
                     }                 
                 }
             }
-            if(!isset($_SESSION['username'])){
-                echo "Tên đăng nhập không tồn tại !";
+            // if(!isset($_SESSION['username'])){
+            //     echo "Đăng nhập không thành công !";
                 
-            }
+            // }
     }
 }
 ?>
